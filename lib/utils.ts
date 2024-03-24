435fdec3a145e71def5551c86361fd4f4ac01abb
triumphbrandstory <triumphbrandstory@gmail.com>
2024-03-23 22:25:56 -0300

    feat: add date formatter function

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,17 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+export function formatDateToLocal(dateStr: string, locale: string = "en-US") {
+  const date = new Date(dateStr);
+  const options: Intl.DateTimeFormatOptions = {
+    day: "numeric",
+    month: "short",
+    year: "numeric",
+  };
+  const formatter = new Intl.DateTimeFormat(locale, options);
+  return formatter.format(date);
 }
