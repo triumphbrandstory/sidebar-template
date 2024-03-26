80ee363579b3aa3234452019840b5fcdd19cbe04
triumphbrandstory <triumphbrandstory@gmail.com>
2024-03-26 01:41:59 -0300

    style: fix divergent padding

--- app/login/[[...login]]/page.tsx
+++ app/login/[[...login]]/page.tsx
@@ -61,7 +61,7 @@ export default function LogInClerkPage() {
       {/* <UserButton /> */}
       {/* left column */}
       <HomeLeftColumn />
-      <div className="flex flex-col items-center justify-between gap-12 overflow-y-scroll bg-white px-12 py-12 md:max-h-screen xl:gap-0">
+      <div className="flex flex-col items-center justify-between gap-12 bg-white px-12 py-12 md:max-h-screen xl:gap-0">
         <AuthenticateButtons />
         <div className="flex h-full w-full place-items-center justify-center">
           <FormWrapper title="Login">
