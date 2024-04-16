bdbe7009040d3718fb244a54a467c4d482559e0f
triumphbrandstory <triumphbrandstory@gmail.com>
2024-04-16 01:37:40 -0300

    feat: use clerk's functions to handle signed in and signed out status

--- components/AuthenticateButtons.tsx
+++ components/AuthenticateButtons.tsx
@@ -1,26 +1,22 @@
-"use client";
 import Link from "next/link";
 import { Button } from "./ui/button";
 import { UserMenuBase } from "@/app/(dashboard)/_components/user-menu-base";
-import { useUser } from "@clerk/nextjs";
+import { SignedIn, SignedOut } from "@clerk/nextjs";
 
 export function AuthenticateButtons() {
-  const { user } = useUser();
-
   return (
     <div className="flex gap-6 self-end">
-      {user ? (
+      <SignedIn>
         <UserMenuBase />
-      ) : (
-        <>
-          <Button asChild>
-            <Link href="/signup">sign up</Link>
-          </Button>
-          <Button asChild>
-            <Link href="/login">login</Link>
-          </Button>
-        </>
-      )}
+      </SignedIn>
+      <SignedOut>
+        <Button asChild>
+          <Link href="/signup">sign up</Link>
+        </Button>
+        <Button asChild>
+          <Link href="/login">login</Link>
+        </Button>
+      </SignedOut>
     </div>
   );
 }
