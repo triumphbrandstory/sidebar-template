6ea9025d5c497233691ce2f9bc151556fb9e0cf0
triumphbrandstory <triumphbrandstory@gmail.com>
2024-06-12 22:10:01 -0300

    chore: fix broken import

--- db/drizzle.ts
+++ db/drizzle.ts
@@ -1,7 +1,6 @@
 import { neon, type NeonQueryFunction } from "@neondatabase/serverless";
 import { drizzle } from "drizzle-orm/neon-http";
 import * as schema from "./schema";
-import { local } from "@/env";
 
 /**
  * Cache the database connection in development. This avoids creating a new connection on every HMR
@@ -12,6 +11,6 @@ const globalForDb = globalThis as unknown as {
 };
 
 const sql =
-  globalForDb.sql ?? neon(process.env.DATABASE_URL || local.NEON_DATABASE_URL);
+  globalForDb.sql ?? neon(process.env.DATABASE_URL!);
 
 export const db = drizzle(sql, { schema });
