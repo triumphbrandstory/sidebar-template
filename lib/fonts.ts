4ce1b519bf186b48bb2b2b076fc561fe3bb0d661
triumphbrandstory <triumphbrandstory@gmail.com>
2024-06-13 14:09:01 -0300

    chore: update fonts

--- lib/fonts.ts
+++ lib/fonts.ts
@@ -2,6 +2,6 @@ import { Archivo } from "next/font/google";
 
 export const archivo = Archivo({
   subsets: ["latin"],
-  weight: ["300", "400", "600"],
-  variable: "--font-archivo",
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+  fallback: ["sans-serif"],
 });
