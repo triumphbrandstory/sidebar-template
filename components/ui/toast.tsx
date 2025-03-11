980f65a7ca7d2d4926f52af4a9849e3f9ffe2edb
triumphbrandstory <triumphbrandstory@gmail.com>
2025-03-10 23:30:14 -0300

    feat: style destructive toast

--- components/ui/toast.tsx
+++ components/ui/toast.tsx
@@ -33,7 +33,7 @@ const toastVariants = cva(
       variant: {
         default: "border-lake-blue bg-background text-lake-blue",
         destructive:
-          "destructive group border-destructive bg-destructive text-destructive-foreground",
+          "destructive group border-destructive bg-background text-destructive",
       },
     },
     defaultVariants: {
@@ -79,7 +79,7 @@ const ToastClose = React.forwardRef<
   <ToastPrimitives.Close
     ref={ref}
     className={cn(
-      "absolute right-1 top-1 rounded-md p-1 text-lake-blue/70 opacity-0 transition-opacity hover:text-lake-blue focus:opacity-100 focus:outline-none focus:ring-1 group-hover:opacity-100 group-[.destructive]:text-red-300 group-[.destructive]:hover:text-red-50 group-[.destructive]:focus:ring-red-400 group-[.destructive]:focus:ring-offset-red-600",
+      "absolute right-1 top-1 rounded-md p-1 text-lake-blue/70 opacity-60 transition-opacity hover:text-lake-blue focus:opacity-100 focus:outline-none focus:ring-1 group-hover:opacity-100 group-[.destructive]:text-red-300 group-[.destructive]:hover:text-red-500 group-[.destructive]:focus:ring-red-400 group-[.destructive]:focus:ring-offset-red-600 [&_.destructive]:text-red-300",
       className,
     )}
     toast-close=""
